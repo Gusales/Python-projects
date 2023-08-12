681000989af215866e8755f6f0dbdb44c9a88f6f
= <=>
2023-08-12 09:18:20 -0300

    fix error at ex006 (double and triple is not **, is *)

--- workouts/ex006.py
+++ workouts/ex006.py
@@ -1,7 +1,7 @@
 # Crie um algoritmo que leia um número e mostre seu dobro, seu triplo e sua raiz quadrada # noqa: E501
 num = int(input('Digite um valor '))
-doub = num**2
-trip = num**3
-raiz = num**(1/2)
+doub = num * 2
+trip = num * 3
+raiz = num ** (1/2)
 
 print(f'O dobro desse número é {doub} \nO triplo desse número é {trip} \nA raiz quadrada desse valor é {raiz}')  # noqa: E501
